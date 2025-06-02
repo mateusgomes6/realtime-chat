a588977de4b76e20bb20243f638dc291b3dc03b8
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-06-02 14:38:16 -0300

    feat: add module to chat

--- src/chat.module.ts
+++ src/chat.module.ts
@@ -4,3 +4,13 @@ import { ChatGateway } from './gateways/chat.gateway';
 import { Message } from './entities/message.entity';
 import { ChatService } from './services/chat.service';
 import { BullModule } from '@nestjs/bull';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Message]),
+    BullModule.registerQueue({ name: 'notifications' }),
+  ],
+  providers: [ChatGateway, ChatService],
+  controllers: [],
+})
+export class ChatModule {}
