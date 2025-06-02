ccd8f69743bb07ddb3c879bf639c58ed71026ab4
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-06-02 15:47:07 -0300

    feat: add jwt guard

---
+++ common/guards/ws-jwt.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { WsException } from '@nestjs/websockets';
+
+@Injectable()
+export class WsJwtGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const client = context.switchToWs().getClient();
+    const token = client.handshake.query.token;
+
+    try {
+      const payload = this.jwtService.verify(token);
+      client.user = payload;  // Anexa o usuário ao cliente
+      return true;
+    } catch (error) {
+      throw new WsException('Token inválido');
+    }
+  }
+}
