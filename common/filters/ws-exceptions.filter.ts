4477ea4d3dd351f9ec8afeb3cd8d3aba9425ccf8
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-06-02 17:49:54 -0300

    feat: add exceptions

---
+++ common/filters/ws-exceptions.filter.ts
@@ -0,0 +1,16 @@
+export const WS_EVENTS = {
+
+  JOIN_ROOM: 'joinRoom',
+  LEAVE_ROOM: 'leaveRoom',
+  SEND_MESSAGE: 'sendMessage',
+  NEW_MESSAGE: 'newMessage',
+  USER_JOINED: 'userJoined',
+  USER_LEFT: 'userLeft',
+
+  ERROR: 'error',
+  UNAUTHORIZED: 'unauthorized',
+};
+
+export const WS_ROOMS = {
+  PREFIX: 'room_',
+};
