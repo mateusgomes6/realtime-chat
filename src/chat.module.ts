265d1538c557f8fdbf87f0ef5bef87cc3b855af4
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-06-02 14:37:42 -0300

    feat: import components

--- src/chat.module.ts
+++ src/chat.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
-export class SrcModule {}
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChatGateway } from './gateways/chat.gateway';
+import { Message } from './entities/message.entity';
+import { ChatService } from './services/chat.service';
+import { BullModule } from '@nestjs/bull';
